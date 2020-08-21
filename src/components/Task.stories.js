651d93beb47cc6840b371b69a3e063755b709289
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2020-08-21 19:01:35 +0100

    cleanup loose ends

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -7,44 +7,43 @@ import Task from "./Task";
   onArchiveTask: { action: "onArchiveTask" },
 }; */
 
-export const taskData = {
+/* export const taskData = {
   id: "1",
   title: "Test Task",
   state: "TASK_INBOX",
   updatedAt: new Date(2018, 0, 1, 9, 0),
-};
+}; */
 export default {
   component: Task,
   // Our exports that end in "Data" are not stories.
-  excludeStories: /.*Data$/,
+  // excludeStories: /.*Data$/,
   title: "Task",
   parameters: {
     assets: [
-     /*  'path/to/your/asset.png',
+      /*  'path/to/your/asset.png',
       'path/to/another/asset.png',
       'path/to/yet/another/asset.png', */
       "./image_1.jpg",
       "./image_2.jpg",
-      "./image_3.jpg"
+      "./image_3.jpg",
     ],
   },
   argTypes: {
     /* ...actionsData, */
-    backgroundColor: { control: 'color' },
+    backgroundColor: { control: "color" },
   },
-  
 };
 
 const Template = (args) => <Task {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  task:{
+  task: {
     id: "1",
     title: "Test Task",
     state: "TASK_INBOX",
     updatedAt: new Date(2018, 0, 1, 9, 0),
-  }
+  },
   //backgroundColor: '#e5f9f7',
 };
 
@@ -60,7 +59,7 @@ Pinned.args = {
 export const Archived = Template.bind({});
 Archived.args = {
   task: {
-   ...Default.args.task,
+    ...Default.args.task,
     state: "TASK_ARCHIVED",
   },
   //backgroundColor: '#d7f5f5',
